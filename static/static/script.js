0b274cf1362cae2238f08578c5009935e1142bb0
miracul9228 <134514965+miracul9228@users.noreply.github.com>
2025-04-23 21:04:22 +0800

    Create script.js

---
+++ static/static/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("showTimeButton");
+  button.addEventListener("click", function () {
+    displayCurrentTime();
+  });
+
+  function displayCurrentTime() {
+    const now = new Date();
+    const timeString = now.toLocaleTimeString();
+    document.getElementById("timeDisplay").innerText =
+      "現在時間: " + timeString;
+  }
+});
